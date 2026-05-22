c40f695cc5d366895aba8f143c9d6caabca1a353
shubhamcodes56 <dixitshubham68359@gmail.com>
2026-05-22 20:42:44 +0530

    utils

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import express from "express";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is not running : $ {process.env.PORT}`);
+    }
+    )
+})
+.catch((err) => {
+    console.log("Failed",err);
+})
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+(async () => {
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("ERRR:",error);
+        throw error
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening $ {process.env.PORT}`);
+       })
+    }
+    catch(error){
+        console.error("ERROR:",error)
+        throw err
+    }
+})()
+*/
+    
